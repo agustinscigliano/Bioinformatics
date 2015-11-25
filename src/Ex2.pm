1220baf058a9ef46774443e54d0651802e863aa1
Julian Gutierrez Ferrara <jugumac@Julians-MacBook-Pro-2.local>
2015-11-24 23:43:08 -0300

    Exercise 3

--- src/Ex2.pm
+++ src/Ex2.pm
@@ -5,7 +5,7 @@ use Bio::SeqIO;
 # This Script creates a BLAST from a FASTA document.
 
 # Subroutine to calculate a BLAST from a FASTA sequence
-# Input: FASTA sequence path
+# Input: FASTA sequence path, a destination path
 # Output: BLAST sequence file.
 # Warning: NCBI remote server is used (Internet required).
 # Usage (from root folder): perl src/Ex2.pm output/1/0F.fa output/2
@@ -16,4 +16,5 @@ sub fastaToBlast{
 	#Calculate and write BLAST sequence
 	write_blast(">$outPath.out",blast_sequence($fasta->next_seq));
 }
+
 fastaToBlast($ARGV[0],$ARGV[1]);
